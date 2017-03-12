6bcdd0fe4789c1a37159ed0a612d2d3147df8d72
Steve Mason <steve@androidbox.me>
2017-03-13 00:34:55 +0700

    Added layout_marginTop=24dp for the include for toolbar to correct the toolbar overlaping the status bar

--- app/src/main/java/me/androidbox/busbymovies/movielist/MovieListActivity.java
+++ app/src/main/java/me/androidbox/busbymovies/movielist/MovieListActivity.java
@@ -15,13 +15,13 @@ public class MovieListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
 
- /*       if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             final Window window = getWindow();
             window.setFlags(
                     WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                     WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
-*/
+
         if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.movielist_container, MovieListViewImp.newInstance(), MovieListViewImp.TAG)
