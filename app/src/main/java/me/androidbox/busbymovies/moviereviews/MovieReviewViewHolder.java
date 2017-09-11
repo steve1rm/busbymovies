966712280055c68bdf6ac3964e13461f0d498baf
Steve Mason <steve@androidbox.me>
2017-09-11 23:22:19 +0700

    Completed reviews speech bubbles with avatars

--- app/src/main/java/me/androidbox/busbymovies/moviereviews/MovieReviewViewHolder.java
+++ app/src/main/java/me/androidbox/busbymovies/moviereviews/MovieReviewViewHolder.java
@@ -1,7 +1,11 @@
 package me.androidbox.busbymovies.moviereviews;
 
+import android.content.Context;
+import android.support.annotation.IdRes;
+import android.support.annotation.IntegerRes;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -15,14 +19,30 @@ import me.androidbox.busbymovies.models.Review;
 
 public class MovieReviewViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.tvContent) TextView mTvContent;
+    @BindView(R.id.tvInitialReviewer) ImageView mInitialReviewer;
+    private Context context;
 
     public MovieReviewViewHolder(View itemView) {
         super(itemView);
 
+        context = itemView.getContext();
         ButterKnife.bind(MovieReviewViewHolder.this, itemView);
     }
 
     public void populateReviewData(Review review) {
         mTvContent.setText(review.getContent());
+
+        String avatar = appendAvatarWithAuthorsInitial(review.getAuthor());
+
+        final int drawableResourceId = context.getResources().getIdentifier(avatar, "drawable", context.getPackageName());
+        mInitialReviewer.setImageResource(drawableResourceId);
+    }
+
+    private String appendAvatarWithAuthorsInitial(final String author) {
+        final String initial = String.valueOf(author.charAt(0));
+        String avatar = "avatar_";
+        return avatar + initial.toLowerCase();
     }
 }
+
+    // Android Studio 3.0 Beta 5
