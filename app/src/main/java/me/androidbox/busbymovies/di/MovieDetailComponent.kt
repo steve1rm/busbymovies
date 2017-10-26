d99158156464e6f9e5c5c932b65b3be0cc5a46d7
Steve Mason <steve@androidbox.me>
2017-10-26 23:12:09 +0700

    Inject ImageLoader in actors viewholder

---
+++ app/src/main/java/me/androidbox/busbymovies/di/MovieDetailComponent.kt
@@ -0,0 +1,16 @@
+package me.androidbox.busbymovies.di
+
+import dagger.Subcomponent
+import me.androidbox.busbymovies.di.scopes.MovieDetailScope
+import me.androidbox.busbymovies.moviedetails.MovieDetailModelImp
+import me.androidbox.busbymovies.moviedetails.MovieDetailViewImp
+
+/**
+ * Created by steve on 10/26/17.
+ */
+@MovieDetailScope
+@Subcomponent(modules = arrayOf(MovieDetailModule::class))
+interface MovieDetailComponent {
+    fun inject(target: MovieDetailModelImp)
+    fun inject(target: MovieDetailViewImp)
+}
