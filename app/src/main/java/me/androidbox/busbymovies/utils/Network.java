e01dc4956592d0aee30eeceb4d3e28c4e53c89b4
Steve Mason <steve@androidbox.me>
2017-11-02 23:08:12 +0700

    Fixed crash in Network.java

--- app/src/main/java/me/androidbox/busbymovies/utils/Network.java
+++ app/src/main/java/me/androidbox/busbymovies/utils/Network.java
@@ -1,18 +1,13 @@
 package me.androidbox.busbymovies.utils;
 
-import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-
-import com.google.common.base.Preconditions;
-
-import org.jetbrains.annotations.NotNull;
+import android.support.v4.util.Preconditions;
 
 import java.io.IOException;
 
-import me.androidbox.busbymovies.di.BusbyMoviesMainApplication;
 import timber.log.Timber;
 
 /**
@@ -20,37 +15,10 @@ import timber.log.Timber;
  */
 
 public final class Network implements IConnectivityProvider {
-    public static boolean isConnectedToNetwork() {
-        final ConnectivityManager connectivityManager =
-                (ConnectivityManager) BusbyMoviesMainApplication.getBusbyInstance().getSystemService(Context.CONNECTIVITY_SERVICE);
-        final NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-
-        return networkInfo != null && networkInfo.isConnected();
-    }
-
-    @Override
-    public boolean isOnline() {
-        final Runtime runtime = Runtime.getRuntime();
-        boolean hasConnected = false;
-
-        try {
-            final Process ipProcess = runtime.exec("/system/bin/ping -c 8.8.8.8");
-            final int exitValue = ipProcess.waitFor();
-
-            hasConnected = (exitValue == 0 || exitValue == 2);
-        }
-        catch(InterruptedException | IOException e) {
-            Timber.e(e.getMessage());
-        }
-
-        return hasConnected;
-    }
-
-
     private @NonNull final ConnectivityManager connectivityManager;
 
     public Network(@NonNull final ConnectivityManager connectivityManager) {
-        this.connectivityManager = Preconditions.checkNotNull(connectivityManager);
+         this.connectivityManager = Preconditions.checkNotNull(connectivityManager);
     }
 
     @Override
@@ -89,4 +57,23 @@ public final class Network implements IConnectivityProvider {
             return type;
         }
     }
+
+    @Override
+    public boolean isOnline() {
+        final Runtime runtime = Runtime.getRuntime();
+        boolean hasConnected = false;
+
+        try {
+            final Process ipProcess = runtime.exec("/system/bin/ping -c 8.8.8.8");
+            final int exitValue = ipProcess.waitFor();
+
+            hasConnected = (exitValue == 0 || exitValue == 2);
+        }
+        catch(InterruptedException | IOException e) {
+            Timber.e(e.getMessage());
+        }
+
+        return hasConnected;
+    }
 }
+
