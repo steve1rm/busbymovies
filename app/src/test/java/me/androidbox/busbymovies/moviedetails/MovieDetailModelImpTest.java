73c8073b8ef8bcb9ffc645f61c33c00135880f5f
= <stevekitkat@gmail.com>
2017-11-21 23:21:56 +0700

    Updated unit tests

--- app/src/test/java/me/androidbox/busbymovies/moviedetails/MovieDetailModelImpTest.java
+++ app/src/test/java/me/androidbox/busbymovies/moviedetails/MovieDetailModelImpTest.java
@@ -5,11 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.inject.Inject;
 
