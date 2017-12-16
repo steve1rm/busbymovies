232416a0922c4656ff2cc31e2417eee3f946a827
= <stevekitkat@gmail.com>
2017-12-16 23:44:38 +0700

    Deleted Reivew java file and replaced with kotlin

--- app/src/main/kotlin/me/androidbox/busbymovies/models/Trailer.kt
+++ app/src/main/kotlin/me/androidbox/busbymovies/models/Trailer.kt
@@ -3,4 +3,7 @@ package me.androidbox.busbymovies.models
 /**
  * Created by steve on 12/14/17.
  */
-data class Trailer(val key: String, val name: String, val size: Int)
+data class Trailer(
+        val key: String = "",
+        val name: String = "",
+        val size: Int = 0)
