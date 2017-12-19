64356e1dc3099f590af0996d88408c40ade4f62a
= <stevekitkat@gmail.com>
2017-12-20 00:38:52 +0700

    Completed code coverage for MovieDetailPresenter

--- app/src/test/java/me/androidbox/busbymovies/moviedetails/MovieDetailPresenterImpTest.java
+++ app/src/test/java/me/androidbox/busbymovies/moviedetails/MovieDetailPresenterImpTest.java
@@ -11,9 +11,14 @@ import java.util.List;
 
 import me.androidbox.busbymovies.models.Actor;
 import me.androidbox.busbymovies.models.Cast;
+import me.androidbox.busbymovies.models.Movie;
 import me.androidbox.busbymovies.models.Movies;
 import me.androidbox.busbymovies.models.Results;
+import me.androidbox.busbymovies.models.Review;
+import me.androidbox.busbymovies.models.Trailer;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
@@ -98,5 +103,182 @@ public class MovieDetailPresenterImpTest {
 
         verify(movieDetailModelContract).getMoveActors(12345, movieDetailPresenter);
         verifyNoMoreInteractions(movieDetailModelContract);
+    }
+
+    @Test
+    public void testRequestMovieReview_doNothing_whenNullValue() {
+        final MovieDetailPresenterImp movieDetailPresenterImp = new MovieDetailPresenterImp(null);
+
+        movieDetailPresenterImp.requestMovieReviews(12345);
+
+        verifyNoMoreInteractions(movieDetailModelContract);
+    }
+
+    @Test
+    public void testRequestMovieReview_getMovieReview() {
+        movieDetailPresenter.requestMovieReviews(12345);
+
+        verify(movieDetailModelContract).getMovieReviews(12345, movieDetailPresenter);
+        verifyNoMoreInteractions(movieDetailModelContract);
+    }
+
+    @Test
+    public void testOnGetMovieReviewsFailure_doNothing_whenNullValue() {
+        movieDetailPresenter.detachView();
+
+        movieDetailPresenter.onGetMovieReviewsFailure("Error Message");
+
+        verifyNoMoreInteractions(movieDetailViewContract);
+    }
+
+    @Test
+    public void testOnGetMovieReviewsFailure_sendErrorMessage_whenNonNullValue() {
+        movieDetailPresenter.onGetMovieReviewsFailure("Error Message");
+
+        verify(movieDetailViewContract).failedToReceiveMovieReviews("Error Message");
+        verifyNoMoreInteractions(movieDetailViewContract);
+    }
+
+    @Test
+    public void testOnGetMovieReviewsSuccess_doNothing_whenNullValue() {
+        movieDetailPresenter.detachView();
+        final Results<Review> reviewList = new Results<>();
+
+        movieDetailPresenter.onGetMovieReviewsSuccess(reviewList);
+
+        verifyNoMoreInteractions(movieDetailViewContract);
+    }
+
+    @Test
+    public void testOnGetMovieReviewsSuccess_sendErrorMessage_whenNonNullValue() {
+        final Results<Review> reviewList = new Results<>();
+
+        movieDetailPresenter.onGetMovieReviewsSuccess(reviewList);
+
+        verify(movieDetailViewContract).receivedMovieReviews(reviewList);
+        verifyNoMoreInteractions(movieDetailViewContract);
+    }
+
+    @Test
+    public void testOnGetMovieTrailerFailure_doNothing_whenNullValue() {
+        movieDetailPresenter.detachView();
+
+        movieDetailPresenter.onGetMovieTrailerFailure("Error Message");
+
+        verifyNoMoreInteractions(movieDetailViewContract);
+    }
+
+    @Test
+    public void testOnGetMovieTrailerFailure_sendErrorMessage_whenNonNullValue() {
+        movieDetailPresenter.onGetMovieTrailerFailure("Error Message");
+
+        verify(movieDetailViewContract).failedToGetMovieTrailers("Error Message");
+        verifyNoMoreInteractions(movieDetailViewContract);
+    }
+
+    @Test
+    public void testOnGetMovieTrailerSuccess_doNothing_whenNullValue() {
+        movieDetailPresenter.detachView();
+        final Results<Trailer> trailerResults = new Results<>();
+
+        movieDetailPresenter.onGetMovieTrailerSuccess(trailerResults);
+
+        verifyNoMoreInteractions(movieDetailViewContract);
+    }
+
+    @Test
+    public void testOnGetMovieTrailerSuccess_sendMovieTrailers_whenNonNullValue() {
+        final Results<Trailer> trailerResults = new Results<>();
+
+        movieDetailPresenter.onGetMovieTrailerSuccess(trailerResults);
+
+        verify(movieDetailViewContract).receivedMovieTrailers(trailerResults);
+        verifyNoMoreInteractions(movieDetailViewContract);
+    }
+
+    @Test
+    public void testRequestMovieTrailer_doNothing_whenNullValue() {
+        final MovieDetailPresenterImp movieDetailPresenterImp = new MovieDetailPresenterImp(null);
+
+        movieDetailPresenterImp.requestMovieTrailer(12345);
+
+        verifyNoMoreInteractions(movieDetailViewContract);
+    }
+
+    @Test
+    public void testRequestMovieTrailer_getMovieTrailer_whenNonNullValue() {
+        final MovieDetailPresenterImp movieDetailPresenterImp = new MovieDetailPresenterImp(movieDetailModelContract);
+
+        movieDetailPresenterImp.requestMovieTrailer(12345);
+
+        verify(movieDetailModelContract).getMovieTrailer(12345, movieDetailPresenterImp);
+        verifyNoMoreInteractions(movieDetailModelContract);
+    }
+
+    @Test
+    public void testOnGetMovieDetailSuccess_doNothing_whenNullValue() {
+        movieDetailPresenter.detachView();
+        final Movie movie = new Movie();
+
+        movieDetailPresenter.onGetMovieDetailSuccess(movie);
+
+        verifyNoMoreInteractions(movieDetailViewContract);
+    }
+
+    @Test
+    public void testOnGetMovieDetailSuccess_displayMovieDetails_whenNonNullValue() {
+        final Movie movie = new Movie();
+
+        movieDetailPresenter.onGetMovieDetailSuccess(movie);
+
+        verify(movieDetailViewContract).displayMovieDetails(movie);
+        verifyNoMoreInteractions(movieDetailViewContract);
+    }
+
+    @Test
+    public void testOnGetMovieDetailFailure_doNothing_whenNullValue() {
+        movieDetailPresenter.detachView();
+
+        movieDetailPresenter.onGetMovieDetailFailure("Error Message");
+
+        verifyNoMoreInteractions(movieDetailViewContract);
+    }
+
+    @Test
+    public void testOnGetMovieDetailFailure_displayErrorFailedToGetMovie_whenNonNullValue() {
+        movieDetailPresenter.onGetMovieDetailFailure("Error Message");
+
+        verify(movieDetailViewContract).displayErrorFailedToGetMovie("Error Message");
+        verifyNoMoreInteractions(movieDetailViewContract);
+    }
+
+    @Test
+    public void testGetMovieDetail_getMovieDetail() {
+        movieDetailPresenter.getMovieDetail(12345);
+
+        verify(movieDetailModelContract).getMovieDetail(12345, movieDetailPresenter);
+        verifyNoMoreInteractions(movieDetailModelContract);
+    }
+
+    @Test
+    public void testDetachView_doNothing_whenNullValue() {
+        movieDetailPresenter.detachView();
+
+        verify(movieDetailModelContract).releaseResources();
+        verifyNoMoreInteractions(movieDetailModelContract);
+
+        movieDetailPresenter.detachView();
+        verifyNoMoreInteractions(movieDetailModelContract);
+    }
+
+    @Test
+    public void testGetVoteAverage_calculatesCorrectVote() {
+        assertThat(movieDetailPresenter.getVoteAverage(7.4F), is(3.7F));
+    }
+
+    @Test
+    public void testGetMovieFormattedDate_formatsDate() {
+        assertThat(movieDetailPresenter.getMovieFormattedDate(
+                "20 Dec 2017", "dd-mmm-yyyy"), is("20 Dec 2017"));
     }
 }
